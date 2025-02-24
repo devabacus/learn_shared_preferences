a1de87d24defc152a9aa8a5af297cc71282a6115
mrfrolk <ekaterita20@gmail.com>
2025-02-24 10:37:00 +0500

    simple withou bloc

---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(App());
+}
+
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomeScreen(),
+    );
+  }
+}
+
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body:Text("Shared preference")
+
+    );
+  }
+}
